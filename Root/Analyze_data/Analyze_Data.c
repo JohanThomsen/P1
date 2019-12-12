f988b44a980108456b9f793d92c85c0ca5264151
jona592e <jzp19@student.aau.dk>
2019-12-12 10:05:31 +0100

    no message

--- Root/Analyze_data/Analyze_Data.c
+++ Root/Analyze_data/Analyze_Data.c
@@ -6,7 +6,7 @@
 
 int main(void)
 {
-    FILE *ifp = fopen("green_energy_48.txt", "r");
+    FILE *ifp = fopen("green_energy.txt", "r");
     char *current_string = (char*)calloc(100, sizeof(char));
     renewable renewable_array[144];
     int start_interval2, end_interval2;
@@ -21,8 +21,8 @@ int main(void)
      4.740, 4.170, 3.630, 3.250, 2.990, 2.880
     };
   
-  printf("Average renewable energy in time interval: %.2f percent\n",renewable_average(renewable_array));
-  printf("Average price in time interval: %.2f DKK/kWh\n",saving_average(price_interval_array, start_interval2, end_interval2, &low_price, &high_price));
+  printf("Average renewable energy in time interval: %f percent\n",renewable_average(renewable_array));
+  printf("\nAverage price in time interval: %.2f DKK/kWh\n",saving_average(price_interval_array, start_interval2, end_interval2, &low_price, &high_price));
 
   printf("Max price in time interval: %.2f DKK/kWh\n",high_price/10);
   printf("Min price in time interval: %.2f DKK/kWh\n",low_price/10);
@@ -33,6 +33,7 @@ int main(void)
   return 0;
 }
 
+/* Reading green_energy and assigning values to the renewable struct, which the renewable_array is created from */
 void read_file(FILE *ifp, char *current_string, renewable *renewable_array)
 {
   int i=0, j=0;
@@ -64,6 +65,7 @@ void read_file(FILE *ifp, char *current_string, renewable *renewable_array)
   }
 }
 
+/* Adding up renewable production in specified interval, diving to find average */ 
 double renewable_average (renewable *renewable_array)
 {
   double renewable_sum=0;
@@ -74,6 +76,7 @@ double renewable_average (renewable *renewable_array)
   
   for (i=start_interval;i < end_interval; i++)
     {
+      printf("\n%f",renewable_array[i].other_renewable );
       renewable_sum += (renewable_array[i].hydro_power + renewable_array[i].other_renewable + renewable_array[i].solar_power
 	            + renewable_array[i].onshore_windpower + renewable_array[i].offshore_windpower) / renewable_array[i].total_load;
     }
@@ -82,6 +85,7 @@ double renewable_average (renewable *renewable_array)
   return print_renew;
 }
 
+/* Adding up prices in specified interval, dividing to find average and finding highest and lowest price  */
 double saving_average (double price_interval_array[], int start_interval2, int end_interval2, double *temp_low, double *temp_high)
 {
   double sum=0;
